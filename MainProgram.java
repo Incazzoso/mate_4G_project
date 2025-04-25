71489cd5097f99a9e4ebd809fd4529558a03be47
Igor07 <igor.melis@icloud.com>
2025-04-25 16:02:14 +0200

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -149,53 +149,6 @@ public class MainProgram extends JFrame {
     private void mostraRisultato(double risultato) {
         JOptionPane.showMessageDialog(this, "Formula: " + formula + "\nRisultato: " + risultato);
         System.exit(0);
-=======
-    //dichiarazioni
-    JFrame frame;           //il frame
-    JPanel northPanel;      //pannello nord
-    JPanel centerPanel;     //pannello centrale
-    JPanel southPanel;      //pannello sud
-    JLabel questionLabel;   //dove avvengono le domande
-    JButton yesButton;      //se alla domanda la risposta è si
-    JButton noButton;       //se alla domanda la risposta è no
-    JButton exitButton;     //se si vuole uscire dal programma
-    
-
-
-    //costruttore (dove andrà tutto il codice)
-    MainProgram(){
-
-        //inizio dei settings per il Frame
-        frame = new JFrame("calcolo combinatorio");            //titolo al frame
-        frame.setLayout(new BorderLayout());                         //tipo di layout
-        frame.setSize(500, 700);                        //dimensione
-
-
-        //inizzializzazione centerPanel ed i suoi componenti
-        centerPanel = new JPanel(new GridLayout(1,1));     //layout a griglia
-        questionLabel = new JLabel("lorem ipsum lorem ipsum lorem ipsum");                         //label vuota
-
-        //aggiunta elementi al centerPanel
-        centerPanel.add(questionLabel);                              //zona dove verrando mandate le risposte
-
-        //inizzializzazione southPanel ed i suoi componenti
-        southPanel = new JPanel(new GridLayout(1,2));     //layout a griglia
-        yesButton = new JButton("SI");
-        noButton = new JButton("NO");
-
-        //aggiunta elementi al centerPanel
-        southPanel.add(yesButton);                                   //bottone si
-        southPanel.add(noButton);                                    //bottone no
-
-
-        //aggiunte al frame
-        frame.add(centerPanel, BorderLayout.CENTER);
-        frame.add(southPanel, BorderLayout.SOUTH);
-
-        //fine dei settings per il Frame
-        frame.setVisible(true);                                    //window visibile
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);         //operazione di chiusura (esce quando lo si chiude)    
->>>>>>> e1e3f305d3fae6540ec76e7f6caa1a1af73577d3
     }
 
     private void mostraErrore() {
